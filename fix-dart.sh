e885128c6775541c86c4f41d7e29bd4801be9754
jeremaihloo <jeremaihloo1024@gmail.com>
2022-12-15 06:04:21 +0800

    chore(代码): 调整 dart 生成配置

--- fix-dart.sh
+++
@@ -1,7 +0,0 @@
-fcr/fcr.bin "../puupee-api-dart/lib/model/application_localization_configuration_dto.dart" \
-    "values: mapCastOfType<String, dynamic>(json, r'values') ?? const {}," \
-    "values: mapCastOfType<String, Map<String, String>>(json, r'values') ?? const {},"
-
-fcr/fcr.bin "../puupee-api-dart/lib/model/type_api_description_model.dart" \
-    "enumValues: Object.listFromJson(json[r'enumValues']) ?? const []," \
-    "enumValues: json[r'enumValues'] is List ? (json[r'enumValues'] as List).cast<String>() : const [],"
